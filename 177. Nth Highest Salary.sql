4787d92db8255a5ae0f26360e4eff62c2ad985cc
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-03-21 12:08:10 +0530

    Update 177. Nth Highest Salary.sql

--- 177. Nth Highest Salary.sql
+++ 177. Nth Highest Salary.sql
@@ -7,3 +7,6 @@ BEGIN
       
   );
 END
+
+# Offset in SQL is used to eliminate a set of records from a given table in order to retrieve a set of records according to the requirement of the database. Basically, it is used to find a starting point to display a set of rows as a final output.
+# The LIMIT clause is used to specify the number of records to return.
